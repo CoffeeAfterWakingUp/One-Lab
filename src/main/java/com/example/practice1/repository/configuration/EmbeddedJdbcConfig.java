3b0265c0eb77fc3777d44153e331f7e83c2797e6
Olzhik01 <180103273@stu.sdu.edu.kz>
2021-10-24 01:14:28 +0600

    EmbeddedJdbcConfig added

---
+++ src/main/java/com/example/practice1/repository/configuration/EmbeddedJdbcConfig.java
@@ -0,0 +1,23 @@
+package com.example.practice1.repository.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan("com.example.practice1")
+public class EmbeddedJdbcConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("schema.sql")
+                .addScripts("author-data.sql", "book-data.sql", "book_author-data.sql", "review-data.sql")
+                .build();
+    }
+}
