6a9a7af488ad19ed4713b52f2835009f0e4cf6bd
Olzhik01 <180103273@stu.sdu.edu.kz>
2021-10-24 01:19:38 +0600

    Practice1Application updated

--- src/main/java/com/example/practice1/Practice1Application.java
+++ src/main/java/com/example/practice1/Practice1Application.java
@@ -1,12 +1,15 @@
 package com.example.practice1;
 
 import com.example.practice1.service.Service;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
-public class Practice1Application {
+public class Practice1Application implements CommandLineRunner {
     private static Service service;
 
     @Autowired
@@ -16,14 +19,22 @@ public class Practice1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Practice1Application.class, args);
-        service.getAllBooks().forEach(System.out::println);
-        System.out.println(service.getBookByTitle("Life of Pi"));
-        System.out.println(service.getBooksByAuthorName("Yann"));
-        service.getReviewsByBookTitle("The Call of the Wild").forEach(System.out::println);
-        service.getReviewsByReviewerName("John Smith").forEach(System.out::println);
-        service.getReviewsByAuthorName("Yann").forEach(System.out::println);
-        service.getAllAuthors().forEach(System.out::println);
-        service.getAuthorByName("Yann").forEach(System.out::println);
-        System.out.println(service.getAuthorById(2L));
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(service.getAllAuthors());
+        System.out.println(service.getAuthorByName("Stephen"));
+        System.out.println(service.getAuthorById(5L));
+        System.out.println("------------------------------------");
+        System.out.println(service.getBooksByAuthorId(9L));
+        System.out.println(service.getBooksByAuthorName("Stephen"));
+        System.out.println(service.getBooksByTitle("The Talisman"));
+        System.out.println(service.getAllBooks());
+        System.out.println("------------------------------------");
+        System.out.println(service.getReviewsByAuthorName("Stephen"));
+        System.out.println(service.getReviewsByBookTitle("The Talisman"));
+        System.out.println(service.getReviewsByReviewerName("John Smith"));
+
     }
 }
