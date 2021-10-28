9b769c4b1879182944f0336fd4fdcfc8e922e84a
Olzhik01 <180103273@stu.sdu.edu.kz>
2021-10-28 13:53:33 +0600

    book_author-data and review-data sql updated

--- src/main/resources/book_author-data.sql
+++ src/main/resources/book_author-data.sql
@@ -1,31 +1,31 @@
 DELETE
 FROM BOOK_AUTHOR;
 
-INSERT INTO BOOK_AUTHOR (book, author)
+INSERT INTO BOOK_AUTHOR (book_id, author_id)
 VALUES (1, 1);
-INSERT INTO BOOK_AUTHOR (book, author)
+INSERT INTO BOOK_AUTHOR (book_id, author_id)
 VALUES (2, 2);
-INSERT INTO BOOK_AUTHOR (book, author)
+INSERT INTO BOOK_AUTHOR (book_id, author_id)
 VALUES (3, 3);
-INSERT INTO BOOK_AUTHOR (book, author)
+INSERT INTO BOOK_AUTHOR (book_id, author_id)
 VALUES (4, 4);
-INSERT INTO BOOK_AUTHOR (book, author)
+INSERT INTO BOOK_AUTHOR (book_id, author_id)
 VALUES (5, 5);
-INSERT INTO BOOK_AUTHOR (book, author)
+INSERT INTO BOOK_AUTHOR (book_id, author_id)
 VALUES (6, 6);
-INSERT INTO BOOK_AUTHOR (book, author)
+INSERT INTO BOOK_AUTHOR (book_id, author_id)
 VALUES (7, 7);
-INSERT INTO BOOK_AUTHOR (book, author)
+INSERT INTO BOOK_AUTHOR (book_id, author_id)
 VALUES (8, 8);
-INSERT INTO BOOK_AUTHOR (book, author)
+INSERT INTO BOOK_AUTHOR (book_id, author_id)
 VALUES (9, 9);
-INSERT INTO BOOK_AUTHOR (book, author)
+INSERT INTO BOOK_AUTHOR (book_id, author_id)
 VALUES (10, 10);
-INSERT INTO BOOK_AUTHOR (book, author)
+INSERT INTO BOOK_AUTHOR (book_id, author_id)
 VALUES (11, 9);
-INSERT INTO BOOK_AUTHOR (book, author)
+INSERT INTO BOOK_AUTHOR (book_id, author_id)
 VALUES (11, 11);
-INSERT INTO BOOK_AUTHOR (book, author)
+INSERT INTO BOOK_AUTHOR (book_id, author_id)
 VALUES (12, 12);
-INSERT INTO BOOK_AUTHOR (book, author)
+INSERT INTO BOOK_AUTHOR (book_id, author_id)
 VALUES (12, 13);
--- src/main/resources/review-data.sql
+++ src/main/resources/review-data.sql
@@ -1,15 +1,15 @@
 DELETE
 FROM REVIEW;
 
-INSERT INTO REVIEW (id, reviewerName, rating, book)
+INSERT INTO REVIEW (id, reviewer_name, rating, book_id)
 VALUES (1, 'John Smith', 10, 1);
-INSERT INTO REVIEW (id, reviewerName, rating, book)
+INSERT INTO REVIEW (id, reviewer_name, rating, book_id)
 VALUES (2, 'Alan Doe', 8, 1);
-INSERT INTO REVIEW (id, reviewerName, rating, book)
+INSERT INTO REVIEW (id, reviewer_name, rating, book_id)
 VALUES (3, 'John Smith', 6, 11);
-INSERT INTO REVIEW (id, reviewerName, rating, book)
+INSERT INTO REVIEW (id, reviewer_name, rating, book_id)
 VALUES (4, 'Alan Doe', 7, 11);
-INSERT INTO REVIEW (id, reviewerName, rating, book)
+INSERT INTO REVIEW (id, reviewer_name, rating, book_id)
 VALUES (5, 'John Smith', 5, 2);
-INSERT INTO REVIEW (id, reviewerName, rating, book)
+INSERT INTO REVIEW (id, reviewer_name, rating, book_id)
 VALUES (6, 'Alan Doe', 5, 2);
