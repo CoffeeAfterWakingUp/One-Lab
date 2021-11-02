13617175c346b1b5c79596c7adf68ed09aed3d89
Olzhik01 <180103273@stu.sdu.edu.kz>
2021-11-03 00:09:19 +0600

    Practice1Application updated

--- src/main/java/com/example/practice1/Practice1Application.java
+++ src/main/java/com/example/practice1/Practice1Application.java
@@ -1,10 +1,7 @@
 package com.example.practice1;
 
-import com.example.practice1.entity.Author;
 import com.example.practice1.kafka.KafkaProducer;
 import com.example.practice1.service.AuthorService;
-import com.example.practice1.service.BookService;
-import com.example.practice1.service.ReviewService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -15,17 +12,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Practice1Application implements CommandLineRunner {
     private static AuthorService authorService;
-    private static BookService bookService;
-    private static ReviewService reviewService;
-
     private static KafkaProducer kafkaProducer;
 
 
     @Autowired
-    public Practice1Application(AuthorService authorService, BookService bookService, ReviewService reviewService, KafkaProducer kafkaProducer) {
+    public Practice1Application(AuthorService authorService, KafkaProducer kafkaProducer) {
         Practice1Application.authorService = authorService;
-        Practice1Application.bookService = bookService;
-        Practice1Application.reviewService = reviewService;
         Practice1Application.kafkaProducer = kafkaProducer;
     }
 
@@ -38,28 +30,8 @@ public class Practice1Application implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Long id = 1L;
-        Author author = authorService.getAuthorById(id);
-        kafkaProducer.sendAuthor(id, author.toString());
-    }
-
-    private void runAuthorServiceMethods() {
-        authorService.getAllAuthors().forEach(System.out::println);
-        authorService.getAuthorByName("Stephen").forEach(System.out::println);
-        System.out.println(authorService.getAuthorById(1L));
-        authorService.getAuthorsByBookId(9L).forEach(System.out::println);
-    }
-
-    private void runBookServiceMethods() {
-        bookService.getBooksByAuthorId(9L).forEach(System.out::println);
-        bookService.getBooksByAuthorName("Stephen").forEach(System.out::println);
-        bookService.getBooksByTitle("The Talisman").forEach(System.out::println);
-        bookService.getAllBooks().forEach(System.out::println);
-    }
-
-    private void runReviewServiceMethods() {
-        reviewService.getReviewsByAuthorName("Stephen").forEach(System.out::println);
-        reviewService.getReviewsByBookTitle("The Talisman").forEach(System.out::println);
-        reviewService.getReviewsByReviewerName("John Smith").forEach(System.out::println);
+//        Long id = 1L;
+//        Author author = authorService.getAuthorById(id);
+//        kafkaProducer.sendAuthor(id, author.toString());
     }
 }
