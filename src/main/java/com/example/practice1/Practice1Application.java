6be7537b9accf89ac23d02713cbb8d0be01d17d7
Olzhik01 <180103273@stu.sdu.edu.kz>
2021-11-07 02:05:38 +0600

    Practice1Application updated

--- src/main/java/com/example/practice1/Practice1Application.java
+++ src/main/java/com/example/practice1/Practice1Application.java
@@ -1,37 +1,15 @@
 package com.example.practice1;
 
-import com.example.practice1.kafka.KafkaProducer;
-import com.example.practice1.service.AuthorService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Slf4j
 @SpringBootApplication
-public class Practice1Application implements CommandLineRunner {
-    private static AuthorService authorService;
-    private static KafkaProducer kafkaProducer;
-
-
-    @Autowired
-    public Practice1Application(AuthorService authorService, KafkaProducer kafkaProducer) {
-        Practice1Application.authorService = authorService;
-        Practice1Application.kafkaProducer = kafkaProducer;
-    }
-
-
-
+public class Practice1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Practice1Application.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-//        Long id = 1L;
-//        Author author = authorService.getAuthorById(id);
-//        kafkaProducer.sendAuthor(id, author.toString());
-    }
 }
